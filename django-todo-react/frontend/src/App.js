a8d4a332833ef57a5c1648975e0a7bdf3b94eaec
GabriellCabrall <gabrielbielcabral@gmail.com>
2024-01-17 11:03:49 -0300

    Testando a renderização de tarefas

--- django-todo-react/frontend/src/App.js
+++ django-todo-react/frontend/src/App.js
@@ -94,8 +94,22 @@ class App extends Component {
     return (
       <main className="container">
         <h1 className="text-white text-uppercase text-center my-4">Todo App</h1>
-        <div className="row"></div>
+        <div className="row">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button className="btn btn-primary">Add Task</button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
       </main>
     );
   }
 }
+
+export default App;
